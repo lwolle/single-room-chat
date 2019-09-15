0b54803481b45dd7191b2aa5849c3aae42620203
lwolle <lukaswollmann@posteo.de>
2019-09-15 22:06:36 +0200

    Add test for login thunk

--- src/ducks/setUser.js
+++ src/ducks/setUser.js
@@ -1,14 +1,14 @@
 import { ActionType } from "./ActionType";
 
 export const setUser = (userId, userName) => ({
-    type: ActionType.LOGIN,
+    type: ActionType.SET_USER,
     userId,
     userName,
 });
 
 export const reducer = (state, action) => {
     switch (action.type) {
-        case ActionType.LOGIN:
+        case ActionType.SET_USER:
             return {
                 ...state,
                 userId: 'asd',
