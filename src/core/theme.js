bf50cbdbd5f0124c574757c106611a1e02b9d9e9
lwolle <lukaswollmann@posteo.de>
2019-09-16 18:30:00 +0200

    Add theme to host app colors

---
+++ src/core/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+    color: {
+        primary: 'rgb(14,74,163)',
+        secondary: 'rgb(14,96,239)',
+        ownMessage: 'rgb(255,255,255)',
+        foreignMessage: 'rgb(212,212,212)',
+    },
+};
