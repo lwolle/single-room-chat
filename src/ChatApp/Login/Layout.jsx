026675d575ee72fcc8d1f2f5171971b8b0e69dce
lwolle <lukaswollmann@posteo.de>
2019-09-16 18:34:28 +0200

    Use the theme to style the Login page

--- src/ChatApp/Login/Layout.jsx
+++ src/ChatApp/Login/Layout.jsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
+import { theme } from '../../core/theme';
 
 export const Layout = styled.div`
     height: 100%;
     display: flex;
     flex-flow: column nowrap;
-    background: rgb(14,74,163);
+    background: ${ theme.color.primary };
     height: 100%;
     align-items: center;
     justify-content: center;
