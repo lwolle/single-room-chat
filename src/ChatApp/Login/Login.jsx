7a16d50f4f16c3bb38c3731bb9312d2aaa2bd238
lwolle <lukaswollmann@posteo.de>
2019-09-17 18:43:41 +0200

    Move useOnChange hook into separate file

--- src/ChatApp/Login/Login.jsx
+++ src/ChatApp/Login/Login.jsx
@@ -1,27 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Layout } from './Layout';
 import { InputContainer } from './InputContainer';
 import { Button } from '../../core/Button/Button';
 import { Input } from '../../core/InputField/InputField';
+import { useOnChange } from '../hooks/useOnChange';
 
-const useOnChange = () => {
-    const [value, setValue] = useState('');
-
-    const handleChange = (event) => {
-        setValue(event.currentTarget.value);
-    };
-
-    return { value, handleChange };
-};
-
-// @todo memoize?
 export const Login = ({ login }) => {
     const { value, handleChange } = useOnChange();
 
     return (
         <Layout>
             <InputContainer>
-                <Input value={ value } onChange={ handleChange } />
+                <Input placeholder="Enter your Username" value={ value } onChange={ handleChange } />
             </InputContainer>
             { /* eslint-disable react/jsx-no-bind */ }
             <Button onClick={ login.bind(null, value) }>
