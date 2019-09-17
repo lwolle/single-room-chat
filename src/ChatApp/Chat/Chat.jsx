89febad140f0bc333ae92c661aeef0a8b6ed3309
lwolle <lukaswollmann@posteo.de>
2019-09-17 19:23:23 +0200

    Add a Textarea to the Chat window.

--- src/ChatApp/Chat/Chat.jsx
+++ src/ChatApp/Chat/Chat.jsx
@@ -1,20 +1,48 @@
 import React from 'react';
 import { Message } from './Message/Message';
 import { Layout } from './Layout';
+import { Textarea } from '../../core/Textarea/Textarea';
+import { Button } from '../../core/Button/Button';
+import { messages } from '../config/messages';
+import { TextareaContainer } from './StyledTextarea';
 
-const renderMessages = (messages, userId) => (
-    messages.map(message => (
-        <Message
-            key={ message.id }
-            text={ message.text }
-            isMine={ message.creatorId === userId }
-        />))
-);
+export class Chat extends React.PureComponent {
+    state = { messageText: '' };
 
-export const Chat = React.memo(({ messages, userId }) => (
-    <React.Fragment>
-        <Layout>
-            { renderMessages(messages, userId) }
-        </Layout>
-    </React.Fragment>
-));
+    handleChange = (event) => {
+        this.setState({ messageText: event.currentTarget.value });
+    };
+
+    handleSubmit = () => {
+        this.props.sendMessage(this.state.messageText);
+        this.setState({ messageText: '' });
+    };
+
+    renderMessages = (messages, userId) => (
+        messages.map(message => (
+            <Message
+                key={ message.id }
+                text={ message.text }
+                isMine={ message.creatorId === userId }
+            />))
+    );
+
+    render() {
+        return (
+            <React.Fragment>
+                <Layout>
+                    { this.renderMessages(this.props.messages, this.props.userId) }
+                    <TextareaContainer>
+                        <Textarea
+                            onChange={ this.handleChange }
+                            value={ this.state.messageText }
+                        />
+                    </TextareaContainer>
+                    <Button onClick={ this.handleSubmit }>
+                        { messages['chat.submitbutton.label'] }
+                    </Button>
+                </Layout>
+            </React.Fragment>
+        );
+    }
+}
---
+++ src/ChatApp/Chat/StyledTextarea.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const TextareaContainer = styled.div`
+    max-width: 450px;
+    width: 100%;
+    margin: 20px auto;
+    border: solid 2px aqua;
+`;
