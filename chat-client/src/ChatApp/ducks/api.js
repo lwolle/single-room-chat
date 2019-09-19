d74160eb687f277f81b3aa61a49bcacb77350576
lwolle <lukaswollmann@posteo.de>
2019-09-19 08:46:14 +0200

    Add routes to login and receives messages

--- chat-client/src/ChatApp/ducks/api.js
+++ chat-client/src/ChatApp/ducks/api.js
@@ -1,30 +1,23 @@
+import axios from 'axios';
+import {
+    API_LOGIN,
+    API_MESSAGES,
+} from '../config/api';
+
 export const api = {
-    login: async () => ({
-        ok: true,
-        body: {
-            userId: 'harhar',
-        },
-    }),
-    getMessages: async () => ({
-        ok: true,
-        body: {
-            messages: [
-                {
-                    id: 1,
-                    creatorId: 2,
-                    text: 'the-message',
-                },
-                {
-                    id: 2,
-                    creatorId: 2,
-                    text: 'the-message2',
-                },
-                {
-                    id: 3,
-                    creatorId: 2,
-                    text: 'the-message3',
-                },
-            ],
-        },
-    }),
+    login: async (userName) => {
+        return axios({
+            method: 'post',
+            url: API_LOGIN,
+            data: {
+                userName,
+            },
+        });
+    },
+    getMessages: async () => {
+        return axios({
+            method: 'GET',
+            url: API_MESSAGES,
+        });
+    },
 };
