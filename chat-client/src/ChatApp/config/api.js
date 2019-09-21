bf6413f57963aeb185a64aaebd39d1f6dd640f6c
lwolle <lukaswollmann@posteo.de>
2019-09-21 22:20:40 +0200

    Set new urls for message and user resource routes.

--- chat-client/src/ChatApp/config/api.js
+++ chat-client/src/ChatApp/config/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://127.0.0.1:8000/api';
+const USER_SERVICE = 'http://127.0.0.1:8001/api';
+const MESSAGE_SERVICE = 'http://127.0.0.1:8000/api';
 
-export const API_LOGIN = `${ BASE_URL }/users`;
-export const API_MESSAGES = `${ BASE_URL }/messages`;
+export const API_LOGIN = `${ USER_SERVICE }/users`;
+export const API_MESSAGES = `${ MESSAGE_SERVICE }/messages`;
