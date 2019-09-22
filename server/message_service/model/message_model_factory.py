e36ae9e7cf96378b8dc1e8685c9cc7c254287a25
lwolle <lukaswollmann@posteo.de>
2019-09-22 23:49:13 +0200

    Extend the message model to include the creator name

--- server/message_service/model/message_model_factory.py
+++ server/message_service/model/message_model_factory.py
@@ -2,7 +2,8 @@ def convert_results(results):
     return [{
         "text": i['text'],
         "id": str(i['_id']),
-        "creatorId": i['creator_id']
+        "creatorId": i['creator_id'],
+        "creatorName": i['creator_name']
     } for i in results]
 
 
@@ -11,7 +12,7 @@ class MessageModel:
         self.messages = messages
 
     def get_all(self):
-        results = self.messages.find({}, {'id': 1, "text": 1, "creator_id": 1})
+        results = self.messages.find({}, {'id': 1, "text": 1, "creator_id": 1, "creator_name": 1})
 
         return convert_results(results)
 
@@ -19,16 +20,18 @@ class MessageModel:
         insert_result = self.messages.insert_one({
             "text": message['text'],
             "creator_id": message['creatorId'],
+            "creator_name": message['creatorName'],
         })
 
         return {
             "id": str(insert_result.inserted_id),
             "text": message['text'],
             "creatorId": message['creatorId'],
+            "creatorName": message['creatorName'],
         }
 
     def search(self, search_term):
         query = {"$text": {"$search": search_term}}
-        results = self.messages.find(query, {'id': 1, "text": 1, "creator_id": 1})
+        results = self.messages.find(query, {'id': 1, "text": 1, "creator_id": 1, "creator_name": 1})
 
         return convert_results(results)
